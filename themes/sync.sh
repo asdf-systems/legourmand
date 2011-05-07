8202913cfe304c58d80bc5d9260ea5ea4f482938
Alexander Surma <surma@zib.de>
2011-05-07 14:29:39 +0200

    sync.sh should actually use the username given

--- themes/sync.sh
+++ themes/sync.sh
@@ -9,5 +9,5 @@ mv toolbox toolbox_$1
 zip -r toolbox_$1.zip toolbox_$1
 mv toolbox_$1 toolbox
 scp toolbox_$1.zip $1@asdf-systems.de:/var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/
-ssh surma@asdf-systems.de /var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/update_toolbox.sh $1
+ssh $1@asdf-systems.de /var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/update_toolbox.sh $1
 rm toolbox_$1.zip
