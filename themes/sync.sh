1f077caaf8e66b438ce82049a59b2d31a5078e29
Alexander Surma <surma@zib.de>
2011-05-07 14:11:33 +0200

    Modified sync.sh for multiuser capabilities

--- themes/sync.sh
+++ themes/sync.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-zip -r toolbox.zip toolbox
-scp toolbox.zip asdf-systems.de:/var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/
-ssh asdf-systems.de /var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/update_toolbox.sh
+
+if [ "$#" -ne 1 ] ; then
+	echo "Usage: $0 <username>"
+	exit 1
+fi
+
+mv toolbox toolbox_$1
+zip -r toolbox_$1.zip toolbox_$1
+mv toolbox_$1 toolbox
+scp toolbox_$1.zip $1@asdf-systems.de:/var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/
+ssh surma@asdf-systems.de /var/www/projects.asdf-systems.de/gourmand/wordpress/wp-content/themes/update_toolbox.sh $1
+rm toolbox_$1.zip
