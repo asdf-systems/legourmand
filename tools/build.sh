9abce247c57621df68e53604f719cb26cdab0702
Alexander Surma <surma@zib.de>
2011-05-07 14:42:50 +0200

    Added a build script for node.js

---
+++ tools/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function error() {
+	echo "[!] Error: $@"
+	exit 1
+}
+
+NUMCPU=$(cat /proc/cpuinfo | grep cpuid | wc -l)
+
+rm -rf build
+mkdir build
+cd build
+(cd ../node && \
+./configure --prefix=../build && \
+make -j$NUMCPU && \
+make install) || error "Could not compile node.js"
+
+PATH=$(pwd)/bin:$PATH
+(make -j$NUMCPU -C ../node_modules/less) || error "Could not compile less"
+
+
+
---
+++ tools/compile_stylefile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PATH=./build/bin:./node_modules/less/bin:$PATH
+
+lessc ../themes/toolbox/lestylesheet.less
+
