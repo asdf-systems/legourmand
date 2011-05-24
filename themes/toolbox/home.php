624321076b990c70d99efa2dafaab518aec7e54c
Christian Schmiedl <schmiedl@asdf-systems.de>
2011-05-24 23:20:05 +0200

    positionierung fertig

--- themes/toolbox/home.php
+++ themes/toolbox/home.php
@@ -1,12 +1,16 @@
 <?php get_header(); ?>
-
-	<?php /* Start the Loop */ $max_thumbs = 6;
-	while ( have_posts() && $thumb_id < $max_thumbs) {
+	<?php /* Start the Loop */
+	$max_thumbs = 6;
+	$thumb_id = 0;
+	while ( have_posts() && $thumb_id < $max_thumbs) { ?>
+		<div class="home_post" id="home_post_<?php echo $thumb_id; ?>">
+		<?
 		the_post();
 		get_template_part( 'content', get_post_format() );
 		$thumb_id++;
-	}
-	?>
+		?>
+		</div>
+	<?php } ?>
 
 
 <?php get_footer(); ?>
