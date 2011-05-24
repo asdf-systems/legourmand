e48fcee6ba6a8a4d587d9ac7474e22f73f2c8e33
Alexander Surma <surma@zib.de>
2011-05-24 20:08:47 +0200

    Gonna grab Chris' work

--- themes/toolbox/single.php
+++ themes/toolbox/single.php
@@ -11,19 +11,7 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<nav id="nav-above">
-					<h1 class="section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-above -->
-
 				<?php get_template_part( 'content', 'single' ); ?>
-
-				<nav id="nav-below">
-					<h1 class="section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-below -->
 
 				<?php comments_template( '', true ); ?>
 
