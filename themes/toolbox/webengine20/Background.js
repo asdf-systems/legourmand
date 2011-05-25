ec488f67af3c0710d43d6407962109a8528f07d9
Friedrich Wessel @peach <wessel@asdf-systems.de>
2011-05-25 14:48:14 +0200

    start working on BackgroundElement

---
+++ themes/toolbox/webengine20/Background.js
@@ -0,0 +1,5 @@
+asdf_Engine.extend(asdf_Background, adsf_Element);
+
+asdf_Background(id, parent, positionX, positionY, bgColor, width, height, positionType, extra_css_class, initialShow, zIndex){
+	
+}
