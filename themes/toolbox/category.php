4c380c915e5c2360f5c4dfb32e3453dbef85f9e0
Alexander Surma <surma@zib.de>
2011-05-31 16:56:50 +0200

    Fixed category name behaviour

--- themes/toolbox/category.php
+++ themes/toolbox/category.php
@@ -9,13 +9,6 @@ get_header(); ?>
 		<section id="primary">
 			<div id="content" role="main">
 
-				<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Category Archives: %s', 'toolbox' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-					?></h1>
-
-					<?php $categorydesc = category_description(); if ( ! empty( $categorydesc ) ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . $categorydesc . '</div>' ); ?>
-				</header>
 
 				<?php /* Display navigation to next/previous pages when applicable */ ?>
 				<?php if ( $wp_query->max_num_pages > 1 ) : ?>
--- themes/toolbox/content.php
+++ themes/toolbox/content.php
@@ -5,46 +5,35 @@
  */
 ?>
 
+<?php
+	$cats = get_the_category(); // Get all categories
+	$firstcat = $cats[0]; // First category
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( is_home() ) : ?>
-			<h1 class="entry-cat"><?php $categories = get_the_category(); echo $categories[0]->cat_name; // use first cat only ?></h1>
-			<?php the_post_thumbnail('thumbnail', 'class=thumbnail'); ?>
-		<?php endif; ?>
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<div class="hr-header">
+			<hr>
+			<div class="hr-title"><span><?=$firstcat->cat_name;?></span></div>
+		</div>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
 
-		<?php if ( 'post' == $post->post_type ) : ?>
 		<div class="entry-meta">
+		von <?=get_the_author();?> am <?=get_the_date();?>, mit <?=comments_number("keinem Kommentar", "einem Kommentar", "% Kommentaren");?>
 			<?php
-				printf( __( '<span class="sep">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'toolbox' ),
-					get_permalink(),
-					get_the_date( 'c' ),
-					get_the_date(),
-					get_author_posts_url( get_the_author_meta( 'ID' ) ),
-					sprintf( esc_attr__( 'View all posts by %s', 'toolbox' ), get_the_author() ),
-					get_the_author()
-				);
+				/*echo "Permalink: ".get_permalink();
+				echo "Date(std-format): ".get_the_date( 'c' );
+				echo "Date: ".get_the_date();
+				echo "Authorurl: ".get_author_posts_url( get_the_author_meta( 'ID' ) );
+				echo "Author: ".get_the_author();*/
 			?>
 		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for search pages ?>
-	<div class="entry-summary">
-		<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
+		<?php the_content(); ?>
 	</div><!-- .entry-content -->
-	<?php endif; ?>
 
 	<footer class="entry-meta">
-		<!--<span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', 'toolbox' ); ?></span><?php the_category( ', ' ); ?></span>
-		<span class="sep"> | </span>
-		<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'toolbox' ) . '</span>', ', ', '<span class="sep"> | </span>' ); ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'toolbox' ), __( '1 Comment', 'toolbox' ), __( '% Comments', 'toolbox' ) ); ?></span>//-->
-		<?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="sep">|</span> <span class="edit-link">', '</span>' ); ?>
-	</footer><!-- #entry-meta -->
+		SHARE THIS MOTHERFUCKING POST!
+	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
