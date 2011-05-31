74f79989d45979737bbf19b2d9921508be136a7e
Alexander Surma <surma@zib.de>
2011-05-31 14:13:15 +0200

    Designed comments

--- themes/toolbox/comments.php
+++ themes/toolbox/comments.php
@@ -15,48 +15,42 @@ if ( ! function_exists( 'toolbox_comment' ) ) :
  *
  * @since Toolbox 0.4
  */
+$comment_count = 0;
 function toolbox_comment( $comment, $args, $depth ) {
+	global $comment_count;
+	$comment_count += 1;
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
 		case '' :
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<footer>
+			<? if($comment_count > 1): ?>
+				<div class="hr-header">
+					<hr>
+					<!--<div class="hr-title"><span><?php _e('Comments', 'toolbox');?></span></div>//-->
+				</div>
+			<? endif; ?>
+			<table>
+				<tr>
+					<td>
 				<div class="comment-author vcard">
 					<?php echo get_avatar( $comment, 40 ); ?>
-					<?php printf( __( '%s <span class="says">says:</span>', 'toolbox' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 				</div><!-- .comment-author .vcard -->
+				</td>
+				<td>
 				<?php if ( $comment->comment_approved == '0' ) : ?>
 					<em><?php _e( 'Your comment is awaiting moderation.', 'toolbox' ); ?></em>
 					<br />
 				<?php endif; ?>
-
-				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(),  get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
-			</footer>
-
 			<div class="comment-content"><?php comment_text(); ?></div>
+			<div class="comment-meta">POSTED BY: <?=get_comment_author_link();?>  <?=printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(),  get_comment_time() ); ?>
+			</td>
+			</tr>
+			</table>
 
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
 		</article><!-- #comment-##  -->
 
-	<?php
-			break;
-		case 'pingback'  :
-		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'toolbox' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'toolbox'), ' ' ); ?></p>
 	<?php
 			break;
 	endswitch;
