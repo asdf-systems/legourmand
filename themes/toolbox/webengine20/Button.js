ac949f059da16f15dcca02fa2fa7b8c0b73806f9
Friedrich Wessel <wessel@asdf-systems.de>
2011-06-10 17:10:46 +0200

    cross browser check button and text

--- themes/toolbox/webengine20/Button.js
+++ themes/toolbox/webengine20/Button.js
@@ -1,9 +1,9 @@
 asdf_Engine.extend(asdf_Button,asdf_Element);
 
 function asdf_Button(id, parent, positionX, positionY, bgColor, width , height, positionType, extra_css_class, initialShow, zIndex,image_normal, image_active, image_hover){
-	this.mNormal = asdf_Engine.initParameter(image_normal, "Normal Image not set");
-	this.mActive = asdf_Engine.initParameter(image_active, "Active Image not set - set to Image normal", image_normal);
-	this.mHover = asdf_Engine.initParameter(image_active, "Hover Image not set - set to Image normal", image_normal);
+	this.mNormalSrc = asdf_Engine.initParameter(image_normal, "Normal Image not set");
+	this.mActiveSrc = asdf_Engine.initParameter(image_active, "Active Image not set - set to Image normal", image_normal);
+	this.mHoverSrc = asdf_Engine.initParameter(image_active, "Hover Image not set - set to Image normal", image_normal);
 	
 	this.mActive = false;
 	
@@ -23,7 +23,7 @@ asdf_Button.prototype.show = function(){
 	if(this.mDomTreeObject == null){
 		asdf_Panel.superClass.show.call(this);
 		
-	this.mImage = asdf_Engine.createDomObject(this, this.mId+ "_Image", "img", this.mType, this.mExtraClassCSS, this.mNormal);
+	this.mImage = asdf_Engine.createDomObject(this, this.mId+ "_Image", "img", this.mType, this.mExtraClassCSS, this.mNormalSrc);
 	this.mImage.style.left = this.mPosX.getValue();
 	this.mImage.style.top = this.mPosY.getValue();
 	this.mImage.style.zIndex = this.mZIndex-1;
@@ -36,14 +36,14 @@ asdf_Button.prototype.setHoverImage = function(){
 	if(this.mDomTreeObject == null)
         return;
 
-    this.mImage.src = this.mHover;
+    this.mImage.src = this.mHoverSrc;
 }
 
 asdf_Button.prototype.unsetHoverImage = function(){
 	if(!this.mActive){
 		if(this.mDomTreeObject == null)
     	    return;
-    	this.mImage.src = this.mNormal;
+    	this.mImage.src = this.mNormalSrc;
 	}
 	
 }
@@ -52,14 +52,12 @@ asdf_Button.prototype.setActiveImage = function(){
 	if(this.mDomTreeObject == null)
         return;
     this.mActive = true;
-    this.mImage.src = this.mActive;
-    alert("setActive");
+    this.mImage.src = this.mActiveSrc;
 }
 
 asdf_Button.prototype.unsetActiveImage = function(){
 	this.mActive = false;
 	this.unsetHoverImage();
-	alert("setInActive");
 }
 
 asdf_Button.prototype.activate = function(){
