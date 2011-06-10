856a144d0a4ac3b3c68368b3be9ab7403ca79c89
Friedrich Wessel <wessel@asdf-systems.de>
2011-06-10 17:19:18 +0200

    write usage for button

--- themes/toolbox/webengine20/Button.js
+++ themes/toolbox/webengine20/Button.js
@@ -1,5 +1,15 @@
 asdf_Engine.extend(asdf_Button,asdf_Element);
-
+/**
+USAGE: var myButton = new asdf_Button("buttonName", $("#PARENTHTMLID").get(0) OR ASDF_ELEMENT, posX, posY, "backgroundColor", width, height, "positionType", "extraCSS", initialShow, zINdex, "NormalImage.png", "activeImage.png", "hoverImage.png" );
+	Then register click Events with:
+	myButton.registerOnMouseClickEvent(asdf_Engine.bind(Element, "functionName"), true);
+	
+	Example:
+	var button1 = new asdf_Button("button1", $("#mainBody").get(0), 10, 10, "transparent", 200, 50, "absolute", "testclass", true, 510, "../media/button1_inaktiv.png", "../media/button1_aktiv.png");
+	var button2 = new asdf_Button("button2", $("#mainBody").get(0), 100, 10, "transparent", 200, 50, "absolute", "testclass", true, 510, "../media/button2_inaktiv.png", "../media/button2_aktiv.png");
+	button1.registerOnMouseClickEvent(asdf_Engine.bind(button2, "toggleActive"), true);
+	button2.registerOnMouseClickEvent(asdf_Engine.bind(button1, "toggleActive"), true);
+*/
 function asdf_Button(id, parent, positionX, positionY, bgColor, width , height, positionType, extra_css_class, initialShow, zIndex,image_normal, image_active, image_hover){
 	this.mNormalSrc = asdf_Engine.initParameter(image_normal, "Normal Image not set");
 	this.mActiveSrc = asdf_Engine.initParameter(image_active, "Active Image not set - set to Image normal", image_normal);
