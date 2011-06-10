1358c901891af19ebf95f942cfe8a05191d03c22
Friedrich Wessel <wessel@asdf-systems.de>
2011-06-10 17:01:19 +0200

    implemented Button Class

---
+++ themes/toolbox/webengine20/Button.js
@@ -0,0 +1,80 @@
+asdf_Engine.extend(asdf_Button,asdf_Element);
+
+function asdf_Button(id, parent, positionX, positionY, bgColor, width , height, positionType, extra_css_class, initialShow, zIndex,image_normal, image_active, image_hover){
+	this.mNormal = asdf_Engine.initParameter(image_normal, "Normal Image not set");
+	this.mActive = asdf_Engine.initParameter(image_active, "Active Image not set - set to Image normal", image_normal);
+	this.mHover = asdf_Engine.initParameter(image_active, "Hover Image not set - set to Image normal", image_normal);
+	
+	this.mActive = false;
+	
+	asdf_Button.baseConstructor.call(this,id, parent, positionX, positionY, bgColor, width , height, positionType, extra_css_class, initialShow, zIndex);
+		
+	this.registerOnMouseEnterEvent(asdf_Engine.bind(this, "setHoverImage"), true);
+	this.registerOnMouseOutEvent(asdf_Engine.bind(this, "unsetHoverImage"), true);
+	this.registerOnMouseClickEvent(asdf_Engine.bind(this, "toggleActive"), true);
+	
+	
+    
+
+
+}
+
+asdf_Button.prototype.show = function(){
+	if(this.mDomTreeObject == null){
+		asdf_Panel.superClass.show.call(this);
+		
+	this.mImage = asdf_Engine.createDomObject(this, this.mId+ "_Image", "img", this.mType, this.mExtraClassCSS, this.mNormal);
+	this.mImage.style.left = this.mPosX.getValue();
+	this.mImage.style.top = this.mPosY.getValue();
+	this.mImage.style.zIndex = this.mZIndex-1;
+
+			
+	}
+	
+}
+asdf_Button.prototype.setHoverImage = function(){
+	if(this.mDomTreeObject == null)
+        return;
+
+    this.mImage.src = this.mHover;
+}
+
+asdf_Button.prototype.unsetHoverImage = function(){
+	if(!this.mActive){
+		if(this.mDomTreeObject == null)
+    	    return;
+    	this.mImage.src = this.mNormal;
+	}
+	
+}
+
+asdf_Button.prototype.setActiveImage = function(){
+	if(this.mDomTreeObject == null)
+        return;
+    this.mActive = true;
+    this.mImage.src = this.mActive;
+    alert("setActive");
+}
+
+asdf_Button.prototype.unsetActiveImage = function(){
+	this.mActive = false;
+	this.unsetHoverImage();
+	alert("setInActive");
+}
+
+asdf_Button.prototype.activate = function(){
+	this.setActiveImage();
+}
+
+asdf_Button.prototype.deactivate = function(){
+	this.unsetActiveImage();
+}
+
+asdf_Button.prototype.toggleActive = function(){
+	if(this.mActive)
+		this.deactivate();
+	else
+		this.activate();
+}
+
+
