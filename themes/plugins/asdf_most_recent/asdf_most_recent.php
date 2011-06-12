3b7d4aa40f8aa21ace95c6422b9dd7436975bdc3
Christian Schmiedl <schmiedl@asdf-systems.de>
2011-06-12 18:32:53 +0200

    asdf_most_recent-framework steht. content muss noch eingepflegt werden.

--- themes/plugins/asdf_most_recent/asdf_most_recent.php
+++ themes/plugins/asdf_most_recent/asdf_most_recent.php
@@ -9,21 +9,29 @@ Author URI: http://www.asdf-systems.de
 */
  
 function asdf_most_recent() { ?>
-	<div id="asdf_most_recent" class="widget">
-		<div id="amr_buttons">
-			<h1 id="amr_articles_button" class="widget-title">ARTIKEL</h1>
-			<h1 class="widget-title">&middot;</h1>
-			<h1 id="amr_comments_button" class="widget-title">KOMMENTARE</h1>
-			<h1 class="widget-title">&middot;</h1>
-			<h1 id="amr_tweets_button" class="widget-title">TWEETS</h1>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.js" type="text/javascript"></script>
+	<script type="text/javascript" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/asdf_most_recent.js"></script> 
+	<div class="widget">
+		<div class="asdf_most_recent">
+			<div class="buttons">
+				<h1 class="articles widget-title">ARTIKEL</h1>
+				<h1 class="widget-title">&middot;</h1>
+				<h1 class="comments widget-title">KOMMENTARE</h1>
+				<h1 class="widget-title">&middot;</h1>
+				<h1 class="tweets widget-title">TWEETS</h1>
+			</div>
+			<div class="content">
+				<div class="articles">
+					<font> content articles</font>
+				</div>
+				<div class="comments">
+					<font> content comments</font>
+				</div>
+				<div class="tweets">
+					<font> content tweets</font>
+				</div>
+			</div>
 		</div>
-		<div id="amr_articles_content" class="amr_content">
-		</div>
-		<div id="amr_comments_content" class="amr_content">
-		</div>
-		<div id="amr_tweets_content" class="amr_content">
-		</div>
-		<font> text</font>
 	</div>
 <?php }
  
---
+++ themes/toolbox/asdf_most_recent.js
@@ -0,0 +1,16 @@
+function asdf_most_recent_show_content(event) {
+	var classList = ["articles", "comments", "tweets"];
+
+	$.each( classList, function(index, item){
+		if ($(event.currentTarget).hasClass(item)) {
+			$('.asdf_most_recent .content div:visible').hide('fast','linear', function() {
+				$('.asdf_most_recent .content .'+item).show('fast','linear');
+			});
+		}
+	});
+	event.stopPropagation();
+}
+
+$(document).ready(function() {
+   $('.asdf_most_recent .buttons h1').bind('click',  asdf_most_recent_show_content);
+});
